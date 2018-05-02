97d4631bd5690aa9711b946c16f6ae92adeca202
Leo <leovolpatto@gmail.com>
2018-05-02 20:31:43 -0300

    Updating tests

---
+++ IntelipostMiddleware.API/SalePlatformTestAPI/SalePlatformController.cs
@@ -0,0 +1,19 @@
+﻿using IntelipostMiddleware.Integrations.External.SalePlatform.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntelipostMiddleware.API.SalePlatformTestAPI
+{
+    [Route("api/[controller]")]
+    public class SalePlatformController : Controller
+    {
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody]SalePlatformTrackInfo value)
+        {
+            return this.Ok(value);
+        }
+    }
+}
